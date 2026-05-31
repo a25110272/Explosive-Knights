de715965aff3e1019ebeeb41dfd1de7a9e227804
Luis Alejandro Hernandez <a25110272@ceti.mx>
2026-05-31 17:03:21 -0600

    Cambios

--- src/03_ExplosiveKnights.cpp
+++ src/03_ExplosiveKnights.cpp
@@ -1,5 +1,4 @@
 #include <SFML/Graphics.hpp>
-#include <SFML/Audio.hpp>
 #include <iostream>
 
 enum Direccion
@@ -17,37 +16,39 @@ public:
     {
         if (!texturaFrente.loadFromFile("assets/images/rojo_frente.png"))
         {
-            std::cout << "Error: no se pudo cargar assets/images/rojo_frente.png" << std::endl;
+            std::cout << "Error al cargar rojo_frente.png\n";
         }
 
         if (!texturaEspalda.loadFromFile("assets/images/rojo_espalda.png"))
         {
-            std::cout << "Error: no se pudo cargar assets/images/rojo_espalda.png" << std::endl;
+            std::cout << "Error al cargar rojo_espalda.png\n";
         }
 
         if (!texturaDerecha.loadFromFile("assets/images/rojo_derecha.png"))
         {
-            std::cout << "Error: no se pudo cargar assets/images/rojo_derecha.png" << std::endl;
+            std::cout << "Error al cargar rojo_derecha.png\n";
         }
 
         if (!texturaIzquierda.loadFromFile("assets/images/rojo_izquierda.png"))
         {
-            std::cout << "Error: no se pudo cargar assets/images/rojo_izquierda.png" << std::endl;
+            std::cout << "Error al cargar rojo_izquierda.png\n";
         }
 
         direccion = ABAJO;
+        numFrames = 7;
 
         sprite.setTexture(texturaFrente);
 
-        numFrames = 4;
-        frameWidth = texturaFrente.getSize().x / numFrames;
-        frameHeight = texturaFrente.getSize().y;
+        // SPRITESHEET VERTICAL
+        frameWidth = texturaFrente.getSize().x;          // 724
+        frameHeight = texturaFrente.getSize().y / numFrames; // 2172 / 7
 
         sprite.setTextureRect(sf::IntRect(0, 0, frameWidth, frameHeight));
+        sprite.setOrigin(frameWidth / 2.0f, frameHeight / 2.0f);
         sprite.setPosition(position);
 
-        // Cambia la escala si se ve muy grande o muy pequeño
-        sprite.setScale(2.0f, 2.0f);
+        // Ajusta esto si se ve muy grande o muy chico
+        sprite.setScale(0.50f, 0.50f);
     }
 
     void move(float offsetX, float offsetY, Direccion nuevaDireccion)
@@ -55,11 +56,6 @@ public:
         sprite.move(offsetX, offsetY);
         direccion = nuevaDireccion;
         caminando = true;
-    }
-
-    void draw(sf::RenderWindow &window)
-    {
-        window.draw(sprite);
     }
 
     void update()
@@ -72,9 +68,10 @@ public:
             {
                 currentFrame = (currentFrame + 1) % numFrames;
 
+                // AQUÍ LEEMOS EN VERTICAL
                 sprite.setTextureRect(sf::IntRect(
-                    currentFrame * frameWidth,
                     0,
+                    currentFrame * frameHeight,
                     frameWidth,
                     frameHeight
                 ));
@@ -86,6 +83,7 @@ public:
         {
             currentFrame = 0;
 
+            // quieto = primer frame
             sprite.setTextureRect(sf::IntRect(
                 0,
                 0,
@@ -97,9 +95,13 @@ public:
         caminando = false;
     }
 
+    void draw(sf::RenderWindow &window)
+    {
+        window.draw(sprite);
+    }
+
 private:
     sf::Sprite sprite;
-
     sf::Texture texturaFrente;
     sf::Texture texturaEspalda;
     sf::Texture texturaDerecha;
@@ -108,41 +110,43 @@ private:
     sf::Clock clock;
 
     Direccion direccion;
-
     bool caminando = false;
 
-    float frameTime = 0.12f;
     int currentFrame = 0;
-    int numFrames = 4;
+    int numFrames = 7;
     int frameWidth = 0;
     int frameHeight = 0;
+
+    float frameTime = 0.03f;
 
     void cambiarTextura()
     {
         if (direccion == ABAJO)
         {
             sprite.setTexture(texturaFrente);
-            frameWidth = texturaFrente.getSize().x / numFrames;
-            frameHeight = texturaFrente.getSize().y;
+            frameWidth = texturaFrente.getSize().x;
+            frameHeight = texturaFrente.getSize().y / numFrames;
         }
         else if (direccion == ARRIBA)
         {
             sprite.setTexture(texturaEspalda);
-            frameWidth = texturaEspalda.getSize().x / numFrames;
-            frameHeight = texturaEspalda.getSize().y;
+            frameWidth = texturaEspalda.getSize().x;
+            frameHeight = texturaEspalda.getSize().y / numFrames;
         }
         else if (direccion == DERECHA)
         {
             sprite.setTexture(texturaDerecha);
-            frameWidth = texturaDerecha.getSize().x / numFrames;
-            frameHeight = texturaDerecha.getSize().y;
+            frameWidth = texturaDerecha.getSize().x;
+            frameHeight = texturaDerecha.getSize().y / numFrames;
         }
         else if (direccion == IZQUIERDA)
         {
             sprite.setTexture(texturaIzquierda);
-            frameWidth = texturaIzquierda.getSize().x / numFrames;
-            frameHeight = texturaIzquierda.getSize().y;
+            frameWidth = texturaIzquierda.getSize().x;
+            frameHeight = texturaIzquierda.getSize().y / numFrames;
         }
+
+        sprite.setOrigin(frameWidth / 2.0f, frameHeight / 2.0f);
     }
 };
 
@@ -152,13 +156,11 @@ int main()
     window.setFramerateLimit(60);
 
     Personaje caballeroRojo(sf::Vector2f(400, 300));
-
-    float velocidad = 3.0f;
+    float velocidad = 6.0f;
 
     while (window.isOpen())
     {
         sf::Event event;
-
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
@@ -171,18 +173,15 @@ int main()
         {
             caballeroRojo.move(0, -velocidad, ARRIBA);
         }
-
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
         {
             caballeroRojo.move(0, velocidad, ABAJO);
         }
-
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
         {
             caballeroRojo.move(-velocidad, 0, IZQUIERDA);
         }
-
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
         {
             caballeroRojo.move(velocidad, 0, DERECHA);
         }
@@ -190,9 +189,7 @@ int main()
         caballeroRojo.update();
 
         window.clear(sf::Color(35, 35, 45));
-
         caballeroRojo.draw(window);
-
         window.display();
     }
 
